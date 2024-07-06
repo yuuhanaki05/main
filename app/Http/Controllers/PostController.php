903bcbccc19779c1f82be4db7e8ad59253f7ac6f
yuuhanaki05 <yuuhanaki05@gmail.com>
2024-07-06 14:56:42 +0900

    8-4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
-
+    
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post'=> $post ]);
+    }
+    public function create()
+    {
+        return view('posts.create');
+    }
+    
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/'.$post->id);
+    }
 }
 ?>
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,7 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-
+    
+        protected $fillable = [
+        'title',
+        'body'
+    ];
     
     public function getPaginateByLimit(int $limit_count = 5)
     { 
