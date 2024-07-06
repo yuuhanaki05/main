7b7480995cc674df7a48a7d3904ed8b2a725785b
yuuhanaki05 <yuuhanaki05@gmail.com>
2024-07-06 15:37:00 +0900

    8-4

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        
+
+        <title>Blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" >
+
+       
+    </head>
+    <body>
+        <h1 class="title">
+            {{ $post->title }}
+        </h1>
+        <div class='content'>
+                <div class='content_post'>
+                   <h3>本文</h3>
+                    <p class='body'>{{ $post->body }}</p>
+                <div>
+       </div>
+       <div class='footer'>
+           <a href="/">戻る</a>
+    </body>
+</html>
